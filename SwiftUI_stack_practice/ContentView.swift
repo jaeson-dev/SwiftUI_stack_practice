e565c675ff7b2af07e46634571bfa1bada32b5e5
jaeson-dev <jaeson.dev@gmail.com>
2022-02-27 03:23:01 +0900

    Initial Commit

---
+++ SwiftUI_stack_practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_stack_practice
+//
+//  Created by Jaeson.dev on 2022/02/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_stack_practice/SwiftUI_stack_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_stack_practiceApp.swift
+//  SwiftUI_stack_practice
+//
+//  Created by Jaeson.dev on 2022/02/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_stack_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
